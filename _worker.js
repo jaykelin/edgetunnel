56d79ad66f525a466dbc186319a8f6e0288b70c1
Jayke.Lin <jayke.lin@hotmail.com>
2025-07-14 21:40:52 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -43,7 +43,7 @@ let 更新时间 = 3;
 let userIDLow;
 let userIDTime = "";
 let proxyIPPool = [];
-let path = '/?ed=2560';
+let path = '/xy5jp34y?xy=4yru8';
 let 动态UUID;
 let link = [];
 let banHosts = [atob('c3BlZWQuY2xvdWRmbGFyZS5jb20=')];
